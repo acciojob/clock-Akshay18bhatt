6cb97adff51bb99af31b25f28a70edea467782c8
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-05 16:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let timer= document.getElementById("timer");
+setInterval(()=>{
+	let instance= new Date();
+	let date= `${(instance.getMonth())+1}/${instance.getDate()}/${instance.getFullYear()}`;
+	
+	let hrs= instance.getHours();
+	// console.log(hrs);
+	
+	let ampm= hrs>=12?"PM":"AM";
+	
+	hrs= hrs>12?(hrs-12):hrs;
+	// console.log(hrs);
+		let time= `${hrs}:${instance.getMinutes()}:${instance.getSeconds()} ${ampm}`;
+	
+	timer.innerText= `${date}, ${time}`;
+	
+	
+},1000);
